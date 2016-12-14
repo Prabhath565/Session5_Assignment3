554e3761ea4b194ecee72545017b7faecc478343
Prabhath565 <prabhath565@gmail.com>
2016-12-14 22:20:59 +0530

    S5_3

---
+++ external.js
@@ -0,0 +1,19 @@
+var details = {
+			Employee: "Employee",
+			number: 815,
+			personalinfo: {
+					 Name: "Prabhath",
+					  Age: 23,
+					  Salary: 30000,
+					  Address:"Madhuaranagra Hyd"
+			},
+			details: {
+					 City: "HYD",
+					 State: "Telangana",
+					 pincode: 5060081
+			 }
+		};
+
+		
+		
+		console.log(details);
